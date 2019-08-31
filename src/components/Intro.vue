5150ce4c1b70a0b562faadf58dfa12bcb0b69711
Vyzaldy Sanchez <vyzaldysanchez@gmail.com>
2019-08-31 03:18:38 -0400

    REDESIGN - Adds new styling files

---
+++ src/components/Intro.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="resume-intro py-3">
+    <div class="media flex-column flex-md-row align-items-center">
+      <img class="resume-profile-image mb-3 mb-md-0 mr-md-5 ml-md-0 rounded mx-auto" alt="Vyzaldy Sanchez" src="@/assets/images/profile.jpeg" />
+      <div class="media-body text-justify">
+        <p class="mb-0">
+          Passionate software engineer specialized in web development with 4+ years of experience. I focus myself on Full-Stack Web Development using JavaScript.
+          I'm a functional programming, clean code and Cloud Services/Architectures enthusiastic.
+          As an engineer, I've been able to obtain knowledge on many areas regarding software engineering, such as requirement specification, architecture, coding, deployment, delivery and so on.
+        </p>
+        <br />
+        <p>
+          I'm always excited with challenges, and I really enjoy learning new things. Thanks to being a fast learner and enthusiast of teamwork, I'm able to adapt pretty well with teams.
+        </p>
+      </div>
+      <!--//media-body-->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
